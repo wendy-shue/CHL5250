dcca8a7103a62291b65704d590625de81d2c0ee3
wendy-shue <wendy.shue@mail.utoronto.ca>
2026-06-12 21:15:43 -0400

    added data_cleaning.r

---
+++ scripts/data_cleaning.R
@@ -0,0 +1,21 @@
+library(dplyr)
+library(mice)
+
+data <- read.csv("../synthetic_dm2_final.csv") 
+
+# drop columns with >30% missingness ====
+missing_prop <- colMeans(is.na(data))
+
+drop_cols <- names(missing_prop[missing_prop > 0.3]) |> c("ID","follow_up_days")
+drop_cols  # inspect which ones are getting dropped
+
+data_clean <- data |> dplyr::select(-all_of(drop_cols))
+
+
+# multiple imputation ====
+set.seed(123)
+
+imp <- mice(data, m = 1, maxit = 5, seed = 123, method="pmm")
+saveRDS(imp, file="../imputed.rds")
+
+
